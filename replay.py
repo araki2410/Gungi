ccfd0976171c211bd6aeb71be2a384ca4cdb3d25
araki2410 <araki2410@gmail.com>
2024-10-11 04:17:55 +0900

    FIX: support for new logformat

--- replay.py
+++ replay.py
@@ -1,28 +1,30 @@
 #!/usr/local/bin/python3
 
 import gungi
-import pickle
 from sys import argv 
 
 try:
     filepath = argv[1]
 except:
-    print("$ python3 replay.py Log/2024Oct07_15:06:42_F25")
+    print("$ python3 replay.py Log/2024Oct10_18:15:16_S32")
     print()
-    filepath = "Log/2024Oct07_15:06:42_F25"
+    filepath = "Log/2024Oct10_18:15:16_S32"
 
 gungi = gungi.Gungi()
-score = pickle.load(open(filepath, 'rb'))
+score = open(filepath, 'r').read().split(" ")
 turn = 0
 gungi.show_board()
 
 for i in score:
     input("turn: " + str(turn))
     print("===============")
+    log16 = format(int(i,16), "016b")
+    to_location = gungi.hex2location(hex(int(log16[8:],2)))
+    pID = int(log16[2:8],2)
+    act = int(log16[:2], 2)
     turn += 1
-    y,x,lv,pID = i
+
     piece = gungi.all_piece[pID]
-    location = [y,x,lv]
-    gungi.play_piece(piece,location)
+    gungi.play_piece(piece, to_location)
     gungi.show_board()
     gungi.show_score(piece)
